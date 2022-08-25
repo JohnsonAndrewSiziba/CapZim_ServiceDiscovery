10a207bc8ca2d50ea50063bff8d966f14a73f915
Johnson Andrew Siziba <siziba.uz@outlook.com>
2022-08-25 11:13:05 +0200

    Initial commit

---
+++ src/test/java/com/capzim/capzim_eureka_server/CapzimEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capzim.capzim_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapzimEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
